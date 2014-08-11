b4fad033382e9d594ce13f47e467fe97b2feddd7
arrrghhh <arrrghhh@xdandroid.com>
2014-08-11 01:12:57 -0600

    DIO Build error fixes

--- fs/direct-io.c
+++ fs/direct-io.c
@@ -106,12 +106,6 @@ struct dio {
 	struct bio *bio_list;		/* singly linked via bi_private */
 	struct task_struct *waiter;	/* waiting task (NULL if none) */
 
-	/* AIO related stuff */
-	struct kiocb *iocb;		/* kiocb */
-	int is_async;			/* is IO async ? */
-	int io_error;			/* IO error in completion path */
-	ssize_t result;                 /* IO result */
-
 	/*
 	 * Page fetching state. These variables belong to dio_refill_pages().
 	 */
